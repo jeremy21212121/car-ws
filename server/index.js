7ec8596c17965d4f05166c4ac3629c93fbebb056
Jeremy <me@jeremypoole.ca>
2020-01-05 20:54:03 -0800

    Change port to be above 1024 and avoid needing root. Remove origin/referrer check for running on localhost.

--- server/index.js
+++ server/index.js
@@ -1,19 +1,14 @@
 const server = require('http').createServer();
 const io = require('socket.io')(server);
 
-server.listen(80);
+server.listen(8088);
 
 io.on('connection', (socket) => {
   const { id } = socket;
 
-  const origin = socket.request.headers.referer || socket.request.headers.origin || '';
-
   socket.broadcast.emit('join');
 
   socket.on('params', (params) => {
-    if (!~origin.indexOf('pakastin.github.io')) {
-      return;
-    }
     const {
       x,
       y,
